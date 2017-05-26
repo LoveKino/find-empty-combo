8a08603dd0db2cb510d1e4ca839e34bf1acdf513
loveKino <842914439@qq.com>
2017-05-26 19:18:33 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * There is a list of set, try to find a combo of set which interset is empty set
+ *
+ * {S1, S2, ..., Sn}, find Si ∩ Sj ∩ ... ∩ Sr = Ф
+ */
+
+let findOne = (setList) => {
+};
+
+module.exports = {
+    findOne
+};

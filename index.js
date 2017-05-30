beff2d3a29a1fa0fc1bf5275c8ff7f97c6b0e89f
loveKino <842914439@qq.com>
2017-05-30 22:26:43 +0800

    map

--- index.js
+++ index.js
@@ -6,9 +6,84 @@
  * {S1, S2, ..., Sn}, find Si ∩ Sj ∩ ... ∩ Sr = Ф
  */
 
-let findOne = (setList) => {
+/**
+ *
+ * Define set F = {S1, S2, ..., Sn}.
+ *
+ * Let A be the all elements of set list F.
+ *
+ * Define w(a, S), if a ϵ S, w(a, S) = 1, otherwise w(a, S) = 0.
+ *
+ * Define φ(a) = {S | w(a, S) = 0, S ϵ F};
+ *
+ * Let Vi ϵ φ(ai), 1 <= i <= |A|
+ *
+ * ∩ Vi = Ф (vice versa)
+ *
+ */
+
+/**
+ * set is an array of strings
+ *
+ * setList = {S1, S2, ..., Sn}
+ * itemList = {a1, a2, ..., am}
+ *
+ * O(m * n)
+ *
+ * 1. search item doesn't containing sets
+ * 2. search set doesn't containing items
+ */
+let getContainMatrix = (setList) => {
+    let itemContainMap = {};
+    let itemNotContainMap = {};
+
+    let setListLen = setList.length;
+
+    for (let i = 0; i < setListLen; i++) {
+        let set = setList[i];
+        let setLen = set.length;
+
+        for (let j = 0; j < setLen; j++) {
+            let item = set [j];
+            let map = itemContainMap[item] = itemContainMap[item] || {};
+            map[i] = 1;
+        }
+    }
+
+    // calculate not contain map
+    for (let item in itemContainMap) {
+        let map = itemNotContainMap[item] = {};
+
+        let list = itemContainMap[item];
+        for (let i = 0; i < setListLen; i++) {
+            if (!list[i]) {
+                map[i] = 1;
+            }
+        }
+    }
+
+    return itemContainMap;
+};
+
+let getNotContainMatrix = (itemContainMap, setListLen) => {
+    let itemNotContainMap = {};
+
+    // calculate not contain map
+    for (let item in itemContainMap) {
+        let map = itemNotContainMap[item] = {};
+
+        let list = itemContainMap[item];
+        for (let i = 0; i < setListLen; i++) {
+            if (!list[i]) {
+                map[i] = 1;
+            }
+        }
+    }
+
+    return itemNotContainMap;
 };
 
 module.exports = {
-    findOne
+    getContainMatrix,
+    getNotContainMatrix
 };
--- test/index.js
+++ test/index.js
@@ -1,5 +1,97 @@
 'use strict';
 
+let {
+    getContainMatrix,
+    getNotContainMatrix
+} = require('..');
+
+let assert = require('assert');
+
 describe('index', () => {
-    it('base', () => {});
+    it('base', () => {
+        let containMatrix = getContainMatrix([
+            ['a', 'b'],
+            ['b', 'c']
+        ]);
+
+        let notContainMatrix = getNotContainMatrix(containMatrix, 2);
+
+        assert.deepEqual(containMatrix, {
+            a: {
+                '0': 1
+            },
+            b: {
+                '0': 1,
+                '1': 1
+            },
+            c: {
+                '1': 1
+            }
+        });
+
+        assert.deepEqual(notContainMatrix, {
+            a: {
+                '1': 1
+            },
+            b: {},
+            c: {
+                '0': 1
+            }
+        });
+    });
+
+    it('containMap', () => {
+        let containMatrix = getContainMatrix([
+            ['a'],
+        ]);
+
+        let notContainMatrix = getNotContainMatrix(containMatrix, 1);
+
+        assert.deepEqual(containMatrix, {
+            a: {
+                '0': 1
+            }
+        });
+
+        assert.deepEqual(notContainMatrix, {
+            a: {}
+        });
+    });
+
+    it('containMap2', () => {
+        let containMatrix = getContainMatrix([
+            ['a'],
+            ['b'],
+            ['c']
+        ]);
+
+        let notContainMatrix = getNotContainMatrix(containMatrix, 3);
+
+        assert.deepEqual(containMatrix, {
+            a: {
+                '0': 1
+            },
+            b: {
+                '1': 1
+            },
+            c: {
+                '2': 1
+            }
+        });
+
+        assert.deepEqual(notContainMatrix, {
+            a: {
+                '1': 1,
+                '2': 1
+            },
+            b: {
+                '0': 1,
+                '2': 1
+            },
+            c: {
+                '0': 1,
+                '1': 1
+            }
+        });
+    });
 });
